e82040672a02425e0ca89310f6e223a4fedc0e8b
Nina Martila <nina.martila@divusi.com>
2017-08-09 13:35:55 +0700

    kinerja

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { View, Image } from 'react-native';
 import {
   Container,
@@ -27,13 +27,13 @@ import Izin from './components/Kehadiran/laporanHarian/Izin';
 
 import formPenugasanLembur from './components/formPenugasanLembur';
 import formCuti from './components/formCuti';
-import DetailKehadiran from './components/Kehadiran/DetailKehadiran' ;
-import DetailPersetujuanPribadiRemote from './components/persetujuan/DetailPersetujuanPribadiRemote' ;
+import DetailKehadiran from './components/Kehadiran/DetailKehadiran';
+import DetailPersetujuanPribadiRemote from './components/persetujuan/DetailPersetujuanPribadiRemote';
 
 import DateTimePickerTester from './components/cobacoba/DateTimePickerTester';
 import TimePicker from './components/cobacoba/TimePicker';
 
-import {Router, Scene} from 'react-native-router-flux';
+import { Router, Scene } from 'react-native-router-flux';
 
 import DetailLemburDriver from './components/lemburdriver/DetailLemburDriver';
 import DetailLembur from './components/lembur/DetailLembur';
@@ -46,166 +46,166 @@ import AppLembur from './components/AppLembur';
 import AppSPJ from './components/AppSPJ';
 import AppPersetujuan from './components/AppPersetujuan';
 
-class App extends Component{
+class App extends Component {
 
 
-  renderContent(){
-        return (
-            <Router>
-              <Scene key="root" 
-              navigationBarStyle={styles.navbarStyle}  
-              titleStyle={{color : "#FFF"}}
-                hideNavBar={true}>
-              
-                <Scene
-                key="keyMainMenu"
-                component={MainMenu}
-                title="Si-Akang"
-                initial 
-                />
+  renderContent() {
+    return (
+      <Router>
+        <Scene key="root"
+          navigationBarStyle={styles.navbarStyle}
+          titleStyle={{ color: "#FFF" }}
+          hideNavBar={true}>
 
-                <Scene
-                key="keylogin"
-                component={LoginForm}
-                
-                />
+          <Scene
+            key="keyMainMenu"
+            component={MainMenu}
+            title="Si-Akang"
 
-                <Scene
-                key="keyLemburDriver"
-                component={FormLemburDriver}
-                title="Lembur Driver"
-                />
+          />
 
-                <Scene
-                key="keyGantiPassword"
-                component={GantiPassword}
-                title="Ganti Password LDAP"
-                />
-            
-                <Scene
-                key="keyKehadiran"
-                component={Kehadiran}
-                title="Kehadiran"
-                />
-                
-                <Scene
-                  key = "applemburdriver"
-                  component = { AppLemburDriver }
-                  title= "Laporan Lembur Driver"
-                  
-                />
-                
-                <Scene
-                  key = "applembur"
-                  component = { AppLembur }
-                  title= "Laporan Lembur"
-                />
+          <Scene
+            key="keylogin"
+            component={LoginForm}
 
-                <Scene
-                    key = "appspj"
-                    component = { AppSPJ }
-                    title= "Laporan SPJ"
-                    
-                />
+          />
 
-                <Scene
-                    key = "kinerja"
-                    component = { AppKinerja }
-                    title= "Kinerja" 
-                         
-                />
+          <Scene
+            key="keyLemburDriver"
+            component={FormLemburDriver}
+            title="Lembur Driver"
+          />
 
-                <Scene
-                    key = "persetujuan"
-                    component = { AppPersetujuan }
-                    title= "Persetujuan"                 
-                />
+          <Scene
+            key="keyGantiPassword"
+            component={GantiPassword}
+            title="Ganti Password LDAP"
+          />
 
-                <Scene
-                    key = "detaillemburdriver"
-                    component = { DetailLemburDriver }
-                    title= "Detail Laporan Lembur Driver"               
-                />
+          <Scene
+            key="keyKehadiran"
+            component={Kehadiran}
+            title="Kehadiran"
+          />
 
-                <Scene
-                    key = "detaillembur"
-                    component = { DetailLembur }
-                    title= "Detail Laporan Lembur"               
-                />
+          <Scene
+            key="applemburdriver"
+            component={AppLemburDriver}
+            title="Laporan Lembur Driver"
 
-                <Scene
-                    key = "detailpribadi"
-                    component = { DetailPribadi }
-                    title= "Detail Laporan Pribadi"               
-                />
+          />
 
-                <Scene
-                    key = "detailspj"
-                    component = { DetailSPJ }
-                    title= "Detail Laporan SPJ"               
-                />
+          <Scene
+            key="applembur"
+            component={AppLembur}
+            title="Laporan Lembur"
+          />
 
-                <Scene
-                    key = "detailbawahan"
-                    component = { DetailBawahan }
-                    title= "Detail Activitas Bawahan"               
-                />
+          <Scene
+            key="appspj"
+            component={AppSPJ}
+            title="Laporan SPJ"
 
-                <Scene
-                key="keyLaporanHarian"
-                component={LaporanHarian}
-                title="Laporan Harian"
-                />
+          />
 
-                <Scene
-                key="keyPenugasanLembur"
-                component={formPenugasanLembur}
-                title="Penugasan Lembur"
-                />
+          <Scene
+            key="kinerja"
+            component={AppKinerja}
+            title="Kinerja"
+            initial
+          />
 
-                <Scene
-                key="keyPengajuanCuti"
-                component={formCuti}
-                title="Pengajuan Cuti"
-                />
+          <Scene
+            key="persetujuan"
+            component={AppPersetujuan}
+            title="Persetujuan"
+          />
 
-              <Scene
-                key="keyDetailKehadiran"
-                component={DetailKehadiran}
-                title="Detail Kehadiran"
-                />
+          <Scene
+            key="detaillemburdriver"
+            component={DetailLemburDriver}
+            title="Detail Laporan Lembur Driver"
+          />
 
-              <Scene
-                key="detailRemote"
-                component={DetailPersetujuanPribadiRemote}
-                title="Detail Remote"
-                />
+          <Scene
+            key="detaillembur"
+            component={DetailLembur}
+            title="Detail Laporan Lembur"
+          />
+
+          <Scene
+            key="detailpribadi"
+            component={DetailPribadi}
+            title="Detail Laporan Pribadi"
+          />
+
+          <Scene
+            key="detailspj"
+            component={DetailSPJ}
+            title="Detail Laporan SPJ"
+          />
+
+          <Scene
+            key="detailbawahan"
+            component={DetailBawahan}
+            title="Detail Activitas Bawahan"
+          />
+
+          <Scene
+            key="keyLaporanHarian"
+            component={LaporanHarian}
+            title="Laporan Harian"
+          />
+
+          <Scene
+            key="keyPenugasanLembur"
+            component={formPenugasanLembur}
+            title="Penugasan Lembur"
+          />
+
+          <Scene
+            key="keyPengajuanCuti"
+            component={formCuti}
+            title="Pengajuan Cuti"
+          />
+
+          <Scene
+            key="keyDetailKehadiran"
+            component={DetailKehadiran}
+            title="Detail Kehadiran"
+          />
+
+          <Scene
+            key="detailRemote"
+            component={DetailPersetujuanPribadiRemote}
+            title="Detail Remote"
+          />
 
 
-              </Scene>
-            </Router>
-        );
+        </Scene>
+      </Router>
+    );
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <Container>
         {this.renderContent()}
-        </Container>
+      </Container>
     );
   }
 }
 
-const styles={
-    spinnerPosition:{
+const styles = {
+  spinnerPosition: {
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
     paddingBottom: 200
   },
- navbarStyle:{
-  backgroundColor: 'blue'
- }
+  navbarStyle: {
+    backgroundColor: 'blue'
+  }
 };
 
 
