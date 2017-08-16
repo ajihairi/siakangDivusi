67fd9b67572405dc4bd2df3c53596378c3d5b7c9
hizrawan oka <hizrawan.oka@divusi.com>
2017-08-16 15:04:42 +0700

    F2-A2 : Removing floating buttons

--- src/components/Kehadiran.js
+++ src/components/Kehadiran.js
@@ -4,11 +4,31 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Actions } from 'react-native-router-flux';
-import {Header, Left, Right, Title, Body, Button, Icon} from 'native-base';
+import {Header, Left, Right, Title, Body, Button, Icon,Item,Picker} from 'native-base';
 import BodyKehadiran from './Kehadiran/BodyKehadiran';
 
 export default class Kehadiran extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected1: "key1",
+      selected2: "keyb"
+    };
+  }
+
+  onValueChange(value: string) {
+    this.setState({
+      selected1: value
+    });
+  }
+
+  _onValueChange(value: string) {
+    this.setState({
+      selected2: value
+    });
+  }
+ 
+  render() {
     return (
       <Provider store={createStore(reducers)}>
         <Container>
@@ -21,7 +41,20 @@ export default class Kehadiran extends Component {
             <Body>
               <Title>Kehadiran</Title>
             </Body>
-            <Right/>
+            <Right>
+            <Picker
+            iosHeader="Select one"
+              mode="dropdown"
+              selectedValue={this.state.selected1}
+              onValueChange={this.onValueChange.bind(this)}
+            >
+              <Item label="Ke pasar" value="key0" />
+              <Item label="Apaa gitu" value="key1" />
+              <Item label="Ke jakarta pagi" value="key2" />
+              <Item label="ke garut shubuh" value="key3" />
+              <Item label="ke mertua" value="key4" />
+            </Picker>
+              </Right>
 
           </Header>
           <BodyKehadiran />
